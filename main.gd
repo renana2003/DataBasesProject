afe6a893658d313810d7fa3ba27687947de42a19
Naama-K <62282313+Naama-K@users.noreply.github.com>
2022-06-08 19:31:59 +0300

    Add files via upload

---
+++ main.gd
@@ -0,0 +1,46 @@
+
+[General]
+Version=1
+
+[Preferences]
+Username=
+Password=2125
+Database=
+DateFormat=
+CommitCount=0
+CommitDelay=0
+InitScript=
+
+[Table]
+Owner=KENIGSBU
+Name=MAINTENANCE
+Count=20000
+
+[Record]
+Name=CHECKING_DATE
+Type=DATE
+Size=
+Data=Random(01-06-2020, 01-06-2022)
+Master=
+
+[Record]
+Name=MAINTENANCE_ID
+Type=NUMBER
+Size=
+Data=Sequence(1)
+Master=
+
+[Record]
+Name=FACILITY_ID
+Type=NUMBER
+Size=
+Data=Random(1, 1000)
+Master=
+
+[Record]
+Name=EMPLOYEE_ID
+Type=NUMBER
+Size=
+Data=Random(888888, 908887)
+Master=
+
